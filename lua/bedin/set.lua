f68b7bc05f787fec877de8e6a2b9df84dd7ecb06
Guilherme Bedin <guilherme0bedin@gmail.com>
2023-12-25 14:52:08 +0000

    default tab = 4, yaml = 2

--- lua/bedin/set.lua
+++ lua/bedin/set.lua
@@ -1,9 +1,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -18,4 +18,5 @@ vim.opt.signcolumn = "yes"
 vim.g.mapleader = " "
 
 vim.cmd([[ au BufRead,BufNewFile *.Jenkinsfile set filetype=groovy ]])
+vim.cmd([[ au FileType yaml,yml set tabstop=2 ]])
 vim.opt.clipboard = "unnamedplus"

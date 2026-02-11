7e1fd5e03b13cd3ce81dc73642e84f504892970c
Guilherme Bedin <guilherme0bedin@gmail.com>
2026-02-11 19:25:51 +0000

    feat: add copilot config

--- init.lua
+++ init.lua
@@ -80,6 +80,13 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Copilot configs
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
+-- create a keymap set that uses leader cc to run copilotchat open
+vim.keymap.set('n', '<leader>cc', ':CopilotChatOpen<CR>', { desc = "Open Copilot Chat" })
+
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -868,6 +875,29 @@ require("lazy").setup({
 		end,
 	},
 
+	-- Copilot
+	{
+		"github/copilot.vim"
+	},
+
+	-- Copilot Chat
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		opts = {
+		-- See Configuration section for options
+			headers = {
+				user = '👤 You',
+				    assistant = '🤖 Copilot',
+				    tool = '🔧 Tool',
+			},
+			separator = '━━',
+			auto_insert_mode = true,
+		},
+    },
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
 	--
